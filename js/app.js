d9e82e0ea65b7667168d24b698c4e263fb769828
leshamaybe <vvaleksey@icloud.com>
2022-08-31 20:32:16 +0300

    changes

--- js/app.js
+++ js/app.js
@@ -1,20 +1,66 @@
-const taskInput = document.querySelector('#task-input');
+const taskInput = document.querySelector('.task-input');
+const pushBtn = document.querySelector('.push-btn');
+const deleteBtn = document.querySelector('.delete-btn');
 const taskBox = document.querySelector('.task-box');
+const catBtn = document.querySelector('.cat-btn');
+const catInput = document.querySelector('.cat-input');
+const categories = document.querySelector('.categories');
+const sidebarBtn = document.querySelector('.sidebar-btn');
 
-taskInput.addEventListener('keypress', (e) => {
-    if (e.keyCode === 13) {
-        let data = taskInput.value;
-        taskBox.append(data);
+
+let tasks = taskBox.children;
+let taskNum = 1;
+
+pushBtn.addEventListener('click', (event) => {
+    let data = taskInput.value;
+    if (!data) return;
+
+    createTask(data, taskNum);
+    taskNum++;
+    taskInput.value = '';
+});
+
+taskBox.addEventListener('click', (event) => {
+    let btn = event.target;
+
+    if (btn.classList.contains('delete-btn')) {
+        btn.parentNode.remove();
     }
-})
+});
 
-function createTask(className, text) {
-    let div = document.createElement('div');
-    let input = document.createElement('input');
-    let label = document.createElement('label');
+catBtn.addEventListener('click', () => {
+    let catName = catInput.value;
 
-    div.classList.add('task');
-    taskBox.append(div);
+    if (!catName) return;
+
+    if (categories.children.length === 10) {
+        alert('Максимальное количество категорий: 10');
+        catInput.value = '';
+        return;
+    };
+
+    createCategory(catName);
+
+    catInput.value = '';
+});
+
+function createTask(text, num) {
+    taskBox.insertAdjacentHTML('afterbegin', `
+    <div class="task">
+        <div class="checkbox">
+            <input class="checkbox-input" type="checkbox" id="checkbox${num}">
+            <label class="checkbox-label" for="checkbox${num}">${text}</label>
+        </div>
+        <div class="mark">Uncategorized</div>
+        <button class="material-icons delete-btn">delete</button>
+    </div>
+    `);
+};
+
+function createCategory(btnName) {
+    let sideBtn = document.createElement('button');
+    sideBtn.classList.add('sidebar-btn');
+    sideBtn.innerHTML = btnName;
+
+    categories.append(sideBtn);
 }
-
-createTask();
