e23d7904ba21cc0f22ee9626aaa96f19dfdde0db
leshamaybe <vvaleksey@icloud.com>
2022-09-02 19:15:37 +0300

    changes

--- js/app.js
+++ js/app.js
@@ -2,13 +2,16 @@ const taskInput = document.querySelector('.task-input');
 const pushBtn = document.querySelector('.push-btn');
 const deleteBtn = document.querySelector('.delete-btn');
 const taskBox = document.querySelector('.task-box');
-const catBtn = document.querySelector('.cat-btn');
-const catInput = document.querySelector('.cat-input');
-const categories = document.querySelector('.categories');
-const sidebarBtn = document.querySelector('.sidebar-btn');
+const container = document.querySelector('.container');
 
+const sidebar = document.createElement('div');
+const catBtn = document.createElement('button');
+const catInput = document.createElement('input');
+const newCat = document.createElement('div');
+const categories = document.createElement('div');
+const sidebarAllBtn = document.createElement('button');
+const line = document.createElement('span');
 
-let tasks = taskBox.children;
 let taskNum = 1;
 
 pushBtn.addEventListener('click', (event) => {
@@ -32,7 +35,6 @@ catBtn.addEventListener('click', () => {
     let catName = catInput.value;
 
     if (!catName) return;
-
     if (categories.children.length === 10) {
         alert('Максимальное количество категорий: 10');
         catInput.value = '';
@@ -40,8 +42,13 @@ catBtn.addEventListener('click', () => {
     };
 
     createCategory(catName);
-
     catInput.value = '';
+});
+
+categories.addEventListener('click', event => {
+    if (event.target.classList.contains('sidebar-btn')) {
+        event.target.classList.toggle('active');
+    }
 });
 
 function createTask(text, num) {
@@ -51,16 +58,47 @@ function createTask(text, num) {
             <input class="checkbox-input" type="checkbox" id="checkbox${num}">
             <label class="checkbox-label" for="checkbox${num}">${text}</label>
         </div>
-        <div class="mark">Uncategorized</div>
+        <select class="mark">
+            <option value="1">Uncategorized</option>
+        </select>
         <button class="material-icons delete-btn">delete</button>
     </div>
     `);
 };
 
 function createCategory(btnName) {
-    let sideBtn = document.createElement('button');
-    sideBtn.classList.add('sidebar-btn');
-    sideBtn.innerHTML = btnName;
+    let newSideBtn = document.createElement('button');
+    newSideBtn.classList.add('sidebar-btn');
+    newSideBtn.innerHTML = btnName;
 
-    categories.append(sideBtn);
+    categories.append(newSideBtn);
 }
+
+function createSidebar() {
+    sidebar.classList.add('sidebar');
+    container.prepend(sidebar);
+
+    categories.classList.add('categories');
+    sidebar.prepend(categories);
+
+    newCat.classList.add('new-cat');
+    categories.after(newCat);
+
+    catInput.classList.add('cat-input');
+    catInput.placeholder = 'New category';
+    catInput.type = 'text';
+    newCat.prepend(catInput);
+
+    catBtn.classList.add('cat-btn');
+    catBtn.innerHTML = 'Add';
+    newCat.append(catBtn);
+
+    sidebarAllBtn.classList.add('sidebar-btn', 'active');
+    sidebarAllBtn.innerHTML = 'All';
+    categories.prepend(sidebarAllBtn);
+
+    line.classList.add('line');
+    sidebar.after(line);
+}
+
+createSidebar();
